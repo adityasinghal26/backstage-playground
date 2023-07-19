3376e0b27c021552e0d826bf866928801ce6407c
adityasinghal26 <adiaggarwal26@gmail.com>
2023-07-19 09:29:53 +0000

    Create kind cluster with local registry plugin

--- scripts/setup-backstage.sh
+++ scripts/setup-backstage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Setup a Kubernetes cluster with kind
-kind create cluster --image kindest/node:v1.26.6 --wait 5m --name backstage
+kind create cluster --image kindest/node:v1.26.6 --wait 5m --name backstage --config ./scripts/kind-cluster-config.yaml
