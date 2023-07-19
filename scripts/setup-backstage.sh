bd2ea50f3874aa3fd74ee9f94f22d3b6b79d51e9
adityasinghal26 <adiaggarwal26@gmail.com>
2023-07-19 19:44:09 +0000

    Handled case of already existing cluster

--- scripts/setup-backstage.sh
+++ scripts/setup-backstage.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+KIND_CLUSTER_NAME="backstage"
 
 # Setup a Kubernetes cluster with kind
-kind create cluster --image kindest/node:v1.26.6 --wait 5m --name backstage --config ./scripts/kind-cluster-config.yaml
+if [[ "$(kind get clusters)" = $KIND_CLUSTER_NAME ]]; then
+  echo "Kind cluster $KIND_CLUSTER_NAME already exists."
+else
+  echo "Creating kind cluster $KIND_CLUSTER_NAME"
+  kind create cluster --image kindest/node:v1.26.6 --wait 5m --name backstage --config ./scripts/kind-cluster-config.yaml
+fi
+
+# Check whether cluster context is configured or not
+# if [[ "$(kubectl config get-contexts | grep kind-backstage | awk {'print $2'})" = "kind-$KIND_CLUSTER_NAME" ]]; then
+#  echo "Cluster context already configured."
+# else
+#  mkdir -p $Home/.kube
+#  sudo kind get kubeconfig -n backstage >> $HOME/.kube/config
+#fi
