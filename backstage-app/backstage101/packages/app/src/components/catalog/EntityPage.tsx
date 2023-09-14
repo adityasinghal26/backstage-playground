54a436423bf74d06bf373429a009c1dd2de0cdf7
Sanyam0811 <139875170+Sanyam0811@users.noreply.github.com>
2023-09-14 12:29:06 +0530

    Update EntityPage.tsx

--- backstage-app/backstage101/packages/app/src/components/catalog/EntityPage.tsx
+++ backstage-app/backstage101/packages/app/src/components/catalog/EntityPage.tsx
@@ -57,6 +57,7 @@ import {
 import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
 import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
 import { EntityTodoContent } from '@backstage/plugin-todo';
+import { MSFormContent, hasMSFormsAnnotation } from '@zcmander/backstage-plugin-msforms';
 
 const techdocsContent = (
   <EntityTechdocsContent>
